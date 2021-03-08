5d66fcace47c947d83dd75633119bd723d257b70
akino <akikoba2001@gmail.com>
2021-03-08 16:54:54 +0900

    commit

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import socket
 import traceback
 import settings
+import os
 
 class Server:
     host = settings.SERVER['host']
     port = settings.SERVER['port']
+    backlog = settings.SERVER['listen_backlog']
+    bufsize = settings.SERVER['bufsize']
 
     def run(self):
         print('Running server...')
@@ -12,7 +15,7 @@ class Server:
             # Create server_socket
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.bind((self.host, self.port))
-            server_socket.listen(5)
+            server_socket.listen(self.backlog)
 
             while True:
                 # Waiting for client connection
@@ -23,29 +26,68 @@ class Server:
 
                 try:
                     # Get request
-                    request = Request(connection.recv(4096))
+                    request = Request(connection.recv(self.bufsize))
 
                     # Create response
-                    response_line = 'HTTP/1.1 200 OK \r\n'
-                    response_body = b'<body><h1>Hello World!</h1></body>'
-                    response_header = ''
-                    response_header += 'Connection: close \r\n'
-                    response_header += 'Content-Language: ja \r\n'
-                    response_header += f'Content-Length: {len(response_body)} \r\n'
-                    response_header += 'Content-Type: text/html; charset=UTF-8 \r\n'
-                    response_header += 'Host: Power \r\n'
-                    response = (response_line + response_header + '\r\n').encode('utf-8') + response_body
-                    connection.send(response)
+                    response = Response(request.method, request.target)
+
+                    connection.send(response.get_context())
                 except Exception:
                     traceback.print_exc()
                 finally:
                     connection.close()
         finally:
             print('Stopped server')
-    
+
+class Response():
+    REASON_PHRASES = {
+        200: 'OK',
+        404: 'Not Found'
+    }
+
+    MEDIA_TYPES = {
+        'html': 'text/html; charset=utf-8',
+        'css': 'text/css',
+        'javascript': 'text/javascript',
+        'json': 'application/json',
+        'jpg': 'image/jpg',
+        'png': 'image/png'
+    }
+
+    def __init__(self, method: str, target: str) -> None:
+        self.http_version = 'HTTP/1.1'
+        self.method = method
+        if self.method == 'GET':
+            file_path = settings.SERVER['document_root'] + '/' + target
+            with open(file_path, 'rb') as f:
+                self.body = f.read()
+                self.status_code = 200
+            if '.' in file_path:
+                extension = file_path.rsplit(".", maxsplit=1)[-1]
+                self.content_type = self.MEDIA_TYPES.get(extension, 'application/octet-stream')
+        else:
+            self.body = b''
+            self.status_code = 404
+        self.reason_phrase = self.REASON_PHRASES[self.status_code]
+        self.headers = {
+            'Connection': 'close',
+            'Content-Language': 'ja',
+            'Content-Length': f'{len(self.body)}',
+            'Host': 'Power',
+            'Content-Type': self.content_type
+        }
+
+    def get_context(self) -> bytes:
+        start_line = f'{self.http_version} {self.status_code} {self.reason_phrase}'
+        response_header = ''
+        for field_name, field_value in self.headers.items():
+            response_header += f'{field_name}: {field_value}\r\n'
+        response_body = self.body
+        context = (start_line + response_header + '\r\n').encode('UTF-8') + response_body
+        return context
 
 class Request():
-    def __init__(self, data) -> None:
+    def __init__(self, data: bytes) -> None:
         coverd_start_line, remain = data.split(sep=b'\r\n', maxsplit=1)
         coverd_headers, coverd_body = remain.split(sep=b'\r\n\r\n', maxsplit=1)
 
@@ -55,7 +97,7 @@ class Request():
         # Parse coverd_headers
         self.headers = {}
         for header_field in coverd_headers.decode('UTF-8').split(sep='\r\n'):
-            field_name, field_value = header_field.split(sep=r': ?')
+            field_name, field_value = header_field.split(sep=r': ')
             self.headers[field_name] = field_value
         
         # Parse coverd_body
